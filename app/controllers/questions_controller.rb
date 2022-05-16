34cad9c87c69797a105260e80d0a4884638a4ab3
jack hanna <jack_hanna1@hotmail.com>
2022-05-16 12:38:04 +0200

    first play with rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,12 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+       @answer = "Great !"
+    end
+  end
+end
